b06edfe14a90854f34b6711f030cba425865b855
Simon <simon@simonssoffa.xyz>
2025-03-25 20:54:47 +0100

    document logging provider; make LoggingProvider::_init a class method

--- spendlog/loggingProvider.py
+++ spendlog/loggingProvider.py
@@ -1,5 +1,27 @@
 import logging
 
+# Minimalist singleton to access the logging module
+#
+# Its only purpose is that I don't want to have to think about where and when
+# I run logging.basicConfig().
+#
+# By always getting the logging module through it, I can be sure that
+# logging.basicConfig() has been run with the defaults I want exactly once
+#
+# I tend to use it in the import section of files as a replacement for importing
+# logging directly.
+#
+# I.e, simply replacing this:
+#
+#       import logging
+#
+# with this
+#
+#       from spendlog.loggingProvider import LoggingProvider
+#       logging = LoggingProvider().logging
+#
+# and then using logging as normal
+
 class LoggingProvider:
 
     _instance = None
@@ -9,15 +31,16 @@ class LoggingProvider:
             cls._instance._init()
         return cls._instance
 
-    def _init(self):
+    @classmethod
+    def _init(cls):
         logFormat = "[%(asctime)s] %(funcName)s(), %(filename)s:%(lineno)d (%(levelname)s): \"%(message)s\""
         logging.basicConfig(
             level = logging.WARNING,
             format = logFormat,
             datefmt="%Y-%m-%d"
         )
-        self.logging = logging
-        self.logging.debug(f"LoggingProvider initialized")
+        cls.logging = logging
+        cls.logging.debug(f"LoggingProvider initialized")
 
     @classmethod
     def reset(cls):
